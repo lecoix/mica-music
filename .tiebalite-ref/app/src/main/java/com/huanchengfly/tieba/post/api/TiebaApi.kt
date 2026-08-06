35a500626f60a3f68e9792b09fec94674d8b1539
lecoix <146210907+lecoix@users.noreply.github.com>
2026-08-07 03:24:39 +0800

    Delete .tiebalite-ref directory

--- .tiebalite-ref/app/src/main/java/com/huanchengfly/tieba/post/api/TiebaApi.kt
+++
@@ -1,9 +0,0 @@
-package com.huanchengfly.tieba.post.api
-
-import com.huanchengfly.tieba.post.api.interfaces.ITiebaApi
-import com.huanchengfly.tieba.post.api.interfaces.impls.MixedTiebaApiImpl
-
-object TiebaApi {
-    @JvmStatic
-    fun getInstance(): ITiebaApi = MixedTiebaApiImpl
-}
--- .tiebalite-ref/app/src/main/java/com/huanchengfly/tieba/post/api/retrofit/exception/TiebaLocalException.kt
+++
@@ -1,6 +0,0 @@
-package com.huanchengfly.tieba.post.api.retrofit.exception
-
-class TiebaLocalException(
-        override val code: Int,
-        msg: String
-) : TiebaException(msg)
